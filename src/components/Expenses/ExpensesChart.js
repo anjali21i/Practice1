33d0383fc0e1b70ec1913311db21ee1fa60449dc
Anjali Gupta <anjaliguptajnp21@gmail.com>
2021-09-20 12:48:01 +0530

    commit on master

---
+++ src/components/Expenses/ExpensesChart.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ExpensesChart = (props) => {
+    
+    const chartDataPoints = [
+        { label :'Jec', value: 0},
+        { label :'Feb', value: 0},
+        { label :'Mar', value: 0},
+        { label :'Apr', value: 0},
+        { label :'May', value: 0},
+        { label :'Jun', value: 0},
+        { label :'Jul', value: 0},
+        { label :'Aug', value: 0},
+        { label :'Sep', value: 0},
+        { label :'Oct', value: 0},
+        { label :'Nov', value: 0},
+        { label :'Dec', value: 0},
+    ];
+    return <div>
+
+    </div>
+
+}
+
+export default ExpensesChart;
